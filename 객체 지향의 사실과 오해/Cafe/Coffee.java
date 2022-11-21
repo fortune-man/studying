cd750a5027645793998b2774083247bf4d699c6a
김주형 <wnguddl96@knou.ac.kr>
2022-11-22 00:06:11 +0900

    Add files via upload

---
+++ 객체 지향의 사실과 오해/Cafe/Coffee.java
@@ -0,0 +1,12 @@
+package Cafe;
+
+public class Coffee {
+    private String name;
+    private int price;
+
+    public Coffee(MenuItem menuItem) {
+        this.name = menuItem.getName();
+        this.price = menuItem.cost();
+    }
+
+}
